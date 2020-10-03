399bb560255b24f8fa464c8dbe4b0e72d879feb8
Duc Nguyen T.[4] <duc.nguyen4@asiantech.vn>
2020-10-03 10:20:36 +0700

    setup layout exercise 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,276 @@
 import 'package:flutter/material.dart';
+// import 'package:big_tip/big_tip.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Builder(builder: (BuildContext context) {
+            return IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {},
+            );
+          }),
+          centerTitle: true,
+          title: const Text('Introduce Cake'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
             ),
           ],
         ),
+        body:
+            // Column(
+            //   crossAxisAlignment: CrossAxisAlignment.center,
+            //   children: [
+            //       Align(
+            //         alignment: Alignment.topRight,
+            //         child:Text(
+            //           'New',
+            //           style: TextStyle(color: Colors.blue),
+            //         ),
+            //       ),
+            //       Icon(
+            //         Icons.cake,
+            //         color: Colors.blue,
+            //         size: 80.0,
+            //       ),
+            //       Text(
+            //         'Banana Cake \n 40 \$',
+            //         textAlign: TextAlign.center,
+            //         style: TextStyle(color: Colors.blue),
+            //       )
+            //   ],
+            // ),
+
+            // Stack(
+            //   children: [
+            //     Container(
+            //       child: Positioned(
+            //         top: 20,
+            //         right: 20,
+            //         child: Text(
+            //           'New',
+            //           style: TextStyle(color: Colors.blue),
+            //         ),
+            //       )
+            //     ),
+            //     Container(
+            //       padding: EdgeInsets.all(20.0),
+            //       decoration: BoxDecoration(
+            //         border: Border.all(
+            //           width: 1,
+            //         ),
+            //       ),
+
+            //       child: Column(
+            //         children: [
+            //           Icon(
+            //             Icons.cake,
+            //             color: Colors.blue,
+            //             size: 80.0,
+            //           ),
+            //           Container(
+            //             margin: EdgeInsets.symmetric(vertical: 10.0),
+            //             padding: EdgeInsets.symmetric(vertical: 10.0),
+            //             color: Colors.blue,
+            //             child: Row(
+            //               children: [
+            //                 Expanded(
+            //                   child: Text('Banana Cake \n 40 \$',
+            //                   textAlign: TextAlign.center,
+            //                   style: TextStyle(color: Colors.white),
+            //                 ),
+            //                 )
+            //               ],
+            //             ),
+            //           ),
+            //         ]
+            //       )
+            //     ),
+            //   ]
+            // ),
+
+            Container(
+          margin: EdgeInsets.all(10.0),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    border: Border(
+                  left: BorderSide(
+                    color: Colors.blue,
+                    width: 4.0,
+                  ),
+                )),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Cake News',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemExtent: 170,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 20,
+                  itemBuilder: (context, index) => Card(
+                    child: Stack(children: [
+                      Container(
+                          child: Positioned(
+                        top: 10,
+                        right: 10,
+                        child: Text(
+                          'New',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      )),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.cake,
+                                  color: Colors.blue,
+                                  size: 80.0,
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                                    color: Colors.blue,
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            'Banana Cake \n 40 \$',
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ])),
+                    ]),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    border: Border(
+                  left: BorderSide(
+                    color: Colors.blue,
+                    width: 4.0,
+                  ),
+                )),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Cake is bought a lot',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemBuilder: (context, index) => Card(
+                    child: Stack(children: [
+                      Container(
+                          child: Positioned(
+                        top: 10,
+                        right: 10,
+                        child: Text(
+                          'Hot',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      )),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.cake,
+                                  color: Colors.blue,
+                                  size: 80.0,
+                                ),
+                                Container(
+                                  child: Text(
+                                    'Banana Cake \n 40 \$',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                ),
+                                Divider(),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.shopping_cart,
+                                        color: Colors.blue),
+                                    Text(
+                                      'Add to cart',
+                                      style: TextStyle(color: Colors.blue),
+                                    ),
+                                  ],
+                                ),
+                              ])),
+                    ]),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed, // This is all you need!
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.cake),
+                title: Text('My Cake'),
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.contacts),
+                title: Text('Contact'),
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Settings'),
+                backgroundColor: Colors.blue,
+              ),
+            ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
